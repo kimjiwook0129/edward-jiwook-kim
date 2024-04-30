1108157a3b5f4501b3ce9cf2f59ce80c3dec2620
Jiwook Kim <kimjiwook0129@Jiwooks-MBP.lan>
2024-04-30 00:07:08 -0400

    updated

--- Home.py
+++ Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from langchain.prompts import PromptTemplate
-from datetime import datetime
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
 
 st.set_page_config(
     page_title = "Edward Jiwook Kim",
@@ -9,6 +9,20 @@ st.set_page_config(
 
 st.title("Hi, I'm Edward Kim 👋")
 st.markdown("""""")
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", """
+     You are a friendly chat agent who answers to questions about a person named "Edward Jiwook Kim".
+     Answer the question using only the following context.
+     
+     If you don't know the answer, or the question sounds not relevant to "Edward Jiwook Kim",
+     just say you don't know. Don't make anything up.
+     
+     Context: {context}
+     """
+    ),
+    ("human", "{question}"),
+])
 
 question_to_agent = st.text_input(
     "Do you have any question about me?",
@@ -24,6 +38,14 @@ with st.sidebar:
 if question_to_agent:
     if openai_api_key:
         st.markdown("Allowed to chat!")
+        llm = ChatOpenAI(
+            temperature=0.1,
+            streaming=True,
+        )
+
+        # Write doc related to Edward
+        # Put embeddings in .cache FAISS vectorstore
+        # 
     else:
         with st.sidebar:
             st.error("Please provide OpenAI Key to chat.")
---
+++ chatbot_info_embed.py
@@ -0,0 +1,16 @@
+from langchain.vectorstores.faiss import FAISS
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+
+def embed_file(file):
+
+    OpenAIEmbeddings
+
+    # Get file
+
+    # Split file
+
+    # OpenAIEmbeddings
+
+    # save the embeddings in the folder
+    
